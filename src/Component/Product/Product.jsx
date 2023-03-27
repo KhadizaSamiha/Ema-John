9546bbeaf607776e8ad68d75a3eea23678e4bfeb
KhadizaSamiha <khadizasamiha7@gmail.com>
2023-03-27 19:35:37 +0400

    added header

---
+++ src/Component/Product/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Product.css'
+const Product = () => {
+    return (
+        <div>
+           <h2>All product here</h2> 
+        </div>
+    );
+};
+
+export default Product;
